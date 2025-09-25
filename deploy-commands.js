1f6cf010b240e266e38324db3245615cc5076483
Ajwad <156331426+ajwadpk@users.noreply.github.com>
2025-09-25 20:25:26 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const { REST, Routes } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
-const config = require("./config.json");
 
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
@@ -12,14 +12,14 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: "10" }).setToken(config.token);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
     console.log("🔄 Refreshing application (/) commands...");
 
     await rest.put(
-      Routes.applicationGuildCommands(config.clientId, config.guildId),
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: commands }
     );
 
