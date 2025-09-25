6858b0aa848d8b1c347ce5eb6c35f2b4c3deaf3f
Ajwad <156331426+ajwadpk@users.noreply.github.com>
2025-09-25 21:18:13 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -17,12 +17,10 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 (async () => {
   try {
     console.log("🔄 Refreshing application (/) commands...");
-
     await rest.put(
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: commands }
     );
-
     console.log("✅ Successfully reloaded application (/) commands.");
   } catch (error) {
     console.error(error);
