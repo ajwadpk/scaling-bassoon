d16eb0c55215aa8ecc7b648d8e450bcaa1b5c0ce
Ajwad <156331426+ajwadpk@users.noreply.github.com>
2025-09-25 20:45:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { REST, Routes } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
 
+const http = require("http");
+http.createServer((req, res) => res.end("Bot is running")).listen(process.env.PORT || 3000);
+
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
