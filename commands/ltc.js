6965707e8f8b6e0a6e67214df45b93b1f27ff8d8
Ajwad <156331426+ajwadpk@users.noreply.github.com>
2025-09-27 10:55:15 +0530

    Update ltc.js

--- commands/ltc.js
+++ commands/ltc.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription("Get the Litecoin wallet address"),
   async execute(interaction) {
     const ltcAddress = "ltc1qfc4dye659d8c3qqzlepv4uhx23wz6x0r830nqk";
-    const qrImage = "https://cdn.discordapp.com/attachments/1399760635268567124/1421115914367008928/image0.jpg?ex=68d7dcd5&is=68d68b55&hm=2b4d321d5228e639ee4d428faa974e44a7480c1d7449053ae9a16d1532149f7e&";
+    const qrImage = "https://cdn.discordapp.com/attachments/1399760635268567124/1421366324952567989/20250927_105039.jpg?ex=68d8c60b&is=68d7748b&hm=04f069632514a67c02bb57df3d5af8105090576c5cbd43413a0a519684543f1d&";
 
     const embed = new EmbedBuilder()
       .setColor(0x3498DB)
